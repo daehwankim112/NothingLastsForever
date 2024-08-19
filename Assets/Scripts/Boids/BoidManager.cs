66c813b9faa8c12aa73e9b968234b53fcd97ee22
David Kim <penguinmillion@gmail.com>
2024-08-19 15:53:57 -0700

    feat: More boid options

--- Assets/Scripts/Boids/BoidManager.cs
+++ Assets/Scripts/Boids/BoidManager.cs
@@ -1,5 +1,7 @@
 
+using System.Linq;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 
@@ -16,49 +18,130 @@ using UnityEngine;
 public class BoidManager : MonoBehaviour
 {
     [SerializeField]
+    // Scale the force of the boid manager
     public float BoidFactor = 1.0f;
 
     [SerializeField]
+    // How aggressivly the boids will avoid each other
     public float SeparationFactor = 1.0f;
 
     [SerializeField]
+    // The distance boids will stay away from each other
     public float SeparationDistance = 1.0f;
 
     [SerializeField]
+    // Will the boids avoid boids they can't see
+    public bool SeparationUsesFov = false;
+
+
+    [SerializeField]
+    // How aggressivly boids will match the velocity of boids around them
     public float AlignmentFactor = 1.0f;
 
     [SerializeField]
+    // The max distance for alignment
     public float AlignmentDistance = 1.0f;
 
     [SerializeField]
+    // Will the boids avoid boids they can't see
+    public bool AlignmentUsesFov = false;
+
+
+    [SerializeField]
+    // How aggressivly boids will stick together
     public float CohesionFactor = 1.0f;
 
     [SerializeField]
+    // The max distance for sticking together
     public float CohesionDistance = 1.0f;
 
     [SerializeField]
+    // Will the boids avoid boids they can't see
+    public bool CohesionUsesFov = false;
+
+
+    [SerializeField]
+    // How aggressivly boids will move towards the target
     public float TargetFactor = 1.0f;
 
     [SerializeField]
+    // The max distance for targeting
+    public float MaxTargetDistance = 1.0f;
+
+    [SerializeField]
+    // Will the boids avoid boids they can't see
+    public bool TargetUsesFov = false;
+
+    [SerializeField]
+    // Boid target mode
+    public BoidRuleMode targetMode = BoidRuleMode.Average;
+
+    [SerializeField]
+    // The targets boids will move towards
+    public List<Transform> targets = new();
+
+
+    [SerializeField]
+    // How aggressivly boids will avoid avoids
+    public float AvoidFactor = 1.0f;
+
+    [SerializeField]
+    // The max distance for targeting
+    public float MaxAvoidDistance = 1.0f;
+
+    [SerializeField]
+    // Will the boids avoid boids they can't see
+    public bool AvoidUsesFov = false;
+
+    [SerializeField]
+    // Boid avoid mode
+    public BoidRuleMode avoidMode = BoidRuleMode.Average;
+
+    [SerializeField]
+    // The targets boids will move away from
+    public List<Transform> avoids = new();
+
+
+    [SerializeField]
+    public float boundingDistance = 100.0f;
+
+    [SerializeField]
+    public float boundingEffectDistance = 90.0f;
+
+
+    [SerializeField]
+    // The maximum force the boid manager can apply
     public float maxForce = 10.0f;
 
     [SerializeField]
+    // The minimum force the boid manager can apply
     public float minForce = 1.0f;
 
+
     [SerializeField]
+    // The range of angles boids will see other boids
     public float boidFov = 45.0f;
 
-    [SerializeField]
-    public Transform target = null;
 
     [SerializeField]
-    public List<Transform> boids = new List<Transform>();
+    // The list of boids this manager manages
+    public readonly List<Transform> boids = new();
+
+    private readonly List<Rigidbody> boidRigidbodies = new();
 
 
-
-    private Vector3 boidsAveragePosition = Vector3.zero;
-
-    private Vector3 boidsAverageVelocity = Vector3.zero;
+    /// <summary>
+    /// What boids
+    /// 
+    /// Closest: Each boid will move toward the target closest to it
+    /// 
+    /// Average: Each boid will move toward the average position of all targets
+    /// </summary>
+    public enum BoidRuleMode
+    {
+        Closest,
+        Average
+    }
 
 
 
@@ -66,6 +149,13 @@ public class BoidManager : MonoBehaviour
     {
         UpdateBoids();
     }
+
+
+    private List<int> frameBoids;
+    private List<Vector3> frameBoidPositions;
+    private List<Vector3> frameBoidVelocities;
+    private List<Vector3> frameTargetPositions;
+    private List<Vector3> frameAvoidPositions;
 
 
 
@@ -77,7 +167,12 @@ public class BoidManager : MonoBehaviour
     /// </param>
     public void AddBoid(Transform boid)
     {
+        if (boid == null) return;
+
+        if (!boid.TryGetComponent<Rigidbody>(out Rigidbody boidRigidbody)) return;
+
         boids.Add(boid);
+        boidRigidbodies.Add(boidRigidbody);
     }
 
 
@@ -94,7 +189,10 @@ public class BoidManager : MonoBehaviour
     /// </returns>
     public bool RemoveBoid(Transform boid)
     {
-        return boids.Remove(boid);
+        Rigidbody boidRigidbody = boid.GetComponent<Rigidbody>();
+
+        return boids.Remove(boid)
+            && boidRigidbodies.Remove(boidRigidbody);
     }
 
 
@@ -108,22 +206,15 @@ public class BoidManager : MonoBehaviour
     /// <returns>
     /// The separation force felt by the boid.
     /// </returns>
-    private Vector3 CalculateSeparationForce(Transform boid)
+    private Vector3 CalculateSeparationForce(int boid)
     {
-        Vector3 separationForce = Vector3.zero;
+        Vector3 boidPosition = frameBoidPositions[boid];
+        Vector3 boidVelocity = frameBoidPositions[boid];
 
-        foreach (Transform nearBoid in boids)
-        {
-            // A boid cannot separate from itself
-            if (nearBoid == boid) continue;
+        List<int> validBoids = FilterPositions(frameBoids, frameBoidPositions, boid, SeparationUsesFov, SeparationDistance);
+        Vector3 separationPosition = CalculateWeightedAveragePosition(validBoids, frameBoidPositions, boidPosition, SeparationDistance);
 
-            // Ignore boids that are too far away
-            if ((boid.position - nearBoid.position).sqrMagnitude > SeparationDistance * SeparationDistance) continue;
-
-            if (!BoidVisible(boid, nearBoid)) continue;
-
-            separationForce -= (nearBoid.position - boid.position);
-        }
+        Vector3 separationForce = boidPosition - separationPosition;
 
         return separationForce;
     }
@@ -139,35 +230,24 @@ public class BoidManager : MonoBehaviour
     /// <returns>
     /// The alignment force felt by the boid.
     /// </returns>
-    private Vector3 CalculateAlignmentForce(Transform boid)
+    private Vector3 CalculateAlignmentForce(int boid)
     {
-        if (!boid.TryGetComponent<Rigidbody>(out Rigidbody boidRigidbody)) return Vector3.zero;
-
-
-        Vector3 alignmentForce = Vector3.zero;
+        Vector3 boidPosition = frameBoidPositions[boid];
+        Vector3 boidVelocity = frameBoidPositions[boid];
 
         Vector3 averageNearbyVelocity = Vector3.zero;
 
         float weightSum = 0.0f;
 
-        foreach (Transform nearBoid in boids)
+        List<int> validBoids = FilterPositions(frameBoids, frameBoidPositions, boid, AlignmentUsesFov, AlignmentDistance);
+
+        foreach (int validBoid in validBoids)
         {
-            // A boid cannot get closer to itself
-            if (nearBoid == boid) continue;
-
-            if (!nearBoid.TryGetComponent<Rigidbody>(out Rigidbody nearBoidRigidbody)) continue;
-
-            // Ignore boids that are too far away
-            if ((boid.position - nearBoid.position).sqrMagnitude > AlignmentDistance * AlignmentDistance) continue;
-
-            // Ignore boids outside the boid's fov
-            if (!BoidVisible(boid, nearBoid)) continue;
-
-            float distance = Vector3.Distance(nearBoid.position, boid.position);
+            float distance = Vector3.Distance(frameBoidPositions[validBoid], boidPosition);
 
             float weight = 1.0f - (distance / AlignmentDistance);
 
-            averageNearbyVelocity += weight * nearBoidRigidbody.velocity;
+            averageNearbyVelocity += weight * frameBoidVelocities[validBoid];
 
             weightSum += weight;
         }
@@ -176,7 +256,7 @@ public class BoidManager : MonoBehaviour
 
         averageNearbyVelocity /= weightSum;
 
-        alignmentForce = averageNearbyVelocity - boidRigidbody.velocity;
+        Vector3 alignmentForce = averageNearbyVelocity - frameBoidVelocities[boid];
 
         return alignmentForce;
     }
@@ -193,38 +273,15 @@ public class BoidManager : MonoBehaviour
     /// <returns>
     /// The cohesion force felt by the boid.
     /// </returns>
-    private Vector3 CalculateCohesionForce(Transform boid)
+    private Vector3 CalculateCohesionForce(int boid)
     {
-        Vector3 cohesionForce = Vector3.zero;
+        Vector3 boidPosition = frameBoidPositions[boid];
+        Vector3 boidVelocity = frameBoidPositions[boid];
 
-        Vector3 averageNearbyPositon = Vector3.zero;
+        List<int> validBoids = FilterPositions(frameBoids, frameBoidPositions, boid, CohesionUsesFov, CohesionDistance);
+        Vector3 cohesionPosition = CalculateWeightedAveragePosition(validBoids, frameBoidPositions, boidPosition, CohesionDistance);
 
-        float weightSum = 0.0f;
-
-        foreach (Transform nearBoid in boids)
-        {
-            // A boid cannot get closer to itself
-            if (nearBoid == boid) continue;
-
-            // Ignore boids that are too far away
-            if ((boid.position - nearBoid.position).sqrMagnitude > CohesionDistance * CohesionDistance) continue;
-
-            if (!BoidVisible(boid, nearBoid)) continue;
-
-            float distance = Vector3.Distance(nearBoid.position, boid.position);
-
-            float weight = 1.0f - (distance / CohesionDistance);
-
-            averageNearbyPositon += weight * nearBoid.position;
-
-            weightSum += weight;
-        }
-
-        if (weightSum < 0.01f) return Vector3.zero;
-
-        averageNearbyPositon /= weightSum;
-
-        cohesionForce = averageNearbyPositon - boid.position;
+        Vector3 cohesionForce = cohesionPosition - boidPosition;
 
         return cohesionForce;
     }
@@ -240,11 +297,81 @@ public class BoidManager : MonoBehaviour
     /// <returns>
     /// The target force felt by the boid.
     /// </returns>
-    private Vector3 CalculateTargetForce(Transform boid)
+    private Vector3 CalculateTargetForce(int boid)
     {
-        if (target == null) return Vector3.zero;
+        Vector3 targetPosition;
+        Vector3 boidPosition = frameBoidPositions[boid];
 
-        return (target.position - boid.position).normalized;
+        List<int> validTargets = FilterPositions(Enumerable.Range(0, targets.Count).ToList(), frameTargetPositions, boid, TargetUsesFov, MaxTargetDistance);
+
+        switch (targetMode)
+        {
+            case BoidRuleMode.Average:
+                targetPosition = CalculateWeightedAveragePosition(validTargets, frameTargetPositions, boidPosition, MaxTargetDistance);
+                break;
+
+            case BoidRuleMode.Closest:
+                targetPosition = GetNearestPosition(validTargets, frameTargetPositions, boidPosition);
+                break;
+
+            default:
+                return Vector3.zero;
+        }
+
+        Vector3 targetForce = (targetPosition - boidPosition).normalized;
+
+        return targetForce;
+    }
+
+
+
+    /// <summary>
+    /// Calculate the force directing a boid away from an object to avoid.
+    /// </summary>
+    /// <param name="boid">
+    /// The boid to calculate the avoidant force of.
+    /// </param>
+    /// <returns>
+    /// The avoidant force felt by the boid.
+    /// </returns>
+    private Vector3 CalculateAvoidForce(int boid)
+    {
+        Vector3 avoidPosition;
+        Vector3 boidPosition = frameBoidPositions[boid];
+
+        List<int> validAvoids = FilterPositions(Enumerable.Range(0, avoids.Count).ToList(), frameAvoidPositions, boid, AvoidUsesFov, MaxAvoidDistance);
+
+        switch (avoidMode)
+        {
+            case BoidRuleMode.Average:
+                avoidPosition = CalculateWeightedAveragePosition(validAvoids, frameAvoidPositions, boidPosition, MaxAvoidDistance);
+                break;
+
+            case BoidRuleMode.Closest:
+                avoidPosition = GetNearestPosition(validAvoids, frameAvoidPositions, boidPosition);
+                break;
+
+            default:
+                return Vector3.zero;
+        }
+
+        Vector3 avoidForce = (boidPosition - avoidPosition).normalized;
+
+        return avoidForce;
+    }
+
+
+
+    private Vector3 CalculateBoundingForce(int boid)
+    {
+        Vector3 boidPosition = frameBoidPositions[boid];
+
+        if (boidPosition.sqrMagnitude > boundingEffectDistance * boundingEffectDistance)
+        {
+            return -boidPosition / boundingDistance;
+        }
+
+        return Vector3.zero;
     }
 
 
@@ -255,22 +382,26 @@ public class BoidManager : MonoBehaviour
     /// <param name="boid">
     /// The boid to apply forces to.
     /// </param>
-    private void ApplyForcesToBoid(Transform boid)
+    private void ApplyForcesToBoid(int boid)
     {
-        if (!boid.TryGetComponent<Rigidbody>(out Rigidbody boidRigidbody)) return;
-
         Vector3 separtationForce = CalculateSeparationForce(boid);
         Vector3 alignmentForce = CalculateAlignmentForce(boid);
         Vector3 cohesionForce = CalculateCohesionForce(boid);
         Vector3 targetForce = CalculateTargetForce(boid);
+        Vector3 avoidForce = CalculateAvoidForce(boid);
+        Vector3 boundingForce = CalculateBoundingForce(boid);
 
         Vector3 totalForce = (SeparationFactor * separtationForce)
                            + (AlignmentFactor * alignmentForce)
                            + (CohesionFactor * cohesionForce)
-                           + (TargetFactor * targetForce);
+                           + (TargetFactor * targetForce)
+                           + (AvoidFactor * avoidForce)
+                           + (1000.0f * boundingForce);
 
         // Bound the force magnitude above
         Vector3 appliedForce = Vector3.ClampMagnitude(BoidFactor * totalForce, maxForce);
+
+        Rigidbody boidRigidbody = boidRigidbodies[boid];
 
         // Bound the force magnitude below
         if (appliedForce.sqrMagnitude < 0.001f)
@@ -292,10 +423,17 @@ public class BoidManager : MonoBehaviour
     /// </summary>
     private void UpdateBoids()
     {
-        //boidsAveragePosition = CalculateBoidsAveragePosition();
-        //boidsAverageVelocity = CalculateBoidsAverageVelocity();
+        // Fill frame lists
+        int numBoids = boids.Count;
 
-        foreach (Transform boid in boids)
+        frameBoids = Enumerable.Range(0, numBoids).ToList();
+        frameBoidPositions = boids.Select(boid => boid.position).ToList();
+        frameBoidVelocities = boids.Select(boid => boid.GetComponent<Rigidbody>().velocity).ToList();
+        frameTargetPositions = targets.Select(target => target.position).ToList();
+        frameAvoidPositions = avoids.Select(avoid => avoid.position).ToList();
+
+
+        for (int boid = 0; boid < numBoids; boid++)
         {
             ApplyForcesToBoid(boid);
         }
@@ -303,65 +441,68 @@ public class BoidManager : MonoBehaviour
 
 
 
-    /// <summary>
-    /// Calculate the average position of all the boids.
-    /// </summary>
-    /// <returns>
-    /// The average position of the boids.
-    /// </returns>
-    private Vector3 CalculateBoidsAveragePosition()
+    private Vector3 CalculateWeightedAveragePosition(List<int> indicies, List<Vector3> positions, Vector3 boidPosition, float maxDistance)
     {
-        Vector3 center = Vector3.zero;
+        Vector3 averageNearbyPositon = Vector3.zero;
 
-        foreach(Transform boid in boids)
+        float weightSum = 0.0f;
+
+        foreach (int index in indicies)
         {
-            center += boid.position;
+            Vector3 position = positions[index];
+
+            float distance = (boidPosition - position).magnitude;
+
+            if (distance > maxDistance) continue;
+
+            // Map 0 - maxDistance to 1 - 0
+            float weight = 1.0f - (distance / maxDistance);
+
+            averageNearbyPositon += weight * position;
+
+            weightSum += weight;
         }
 
-        center /= boids.Count;
+        if (weightSum < 0.01f) return boidPosition;
 
-        return center;
+        averageNearbyPositon /= weightSum;
+
+        return averageNearbyPositon;
     }
 
 
 
-    /// <summary>
-    /// Calculate the average velocity of all the boids.
-    /// </summary>
-    /// <returns>
-    /// The average velocity of the boids.
-    /// </returns>
-    private Vector3 CalculateBoidsAverageVelocity()
+    private List<int> FilterPositions(List<int> indicies, List<Vector3> positions, int boid, bool useFov, float maxDistance)
     {
-        Vector3 center = Vector3.zero;
+        float sqrMaxDistance = maxDistance * maxDistance;
 
-        foreach (Transform boid in boids)
-        {
-            if (!boid.TryGetComponent<Rigidbody>(out Rigidbody boidRigidbody)) continue;
+        Vector3 filterBoidPosition = frameBoidPositions[boid];
+        Vector3 filterBoidForward = frameBoidVelocities[boid];
 
-            center += boidRigidbody.velocity;
-        }
-
-        center /= boids.Count;
-
-        return center;
+        return indicies.Where(index => (filterBoidPosition - positions[index]).sqrMagnitude < sqrMaxDistance
+                                    && (!useFov || BoidVisible(filterBoidPosition, filterBoidForward, positions[index]))
+                              )
+                        .ToList();
     }
 
 
 
-    private bool BoidVisible(Transform boid, Transform other)
+    private Vector3 GetNearestPosition(List<int> indicies, List<Vector3> positions, Vector3 boid)
     {
-        if (other == null) return false;
+        if (positions == null || positions.Count == 0) return boid;
 
-        if (!boid.TryGetComponent<Rigidbody>(out Rigidbody boidRigidbody)) return false;
+        return positions[indicies.OrderBy(index => (positions[index] - boid).sqrMagnitude)
+                                 .FirstOrDefault()
+                        ];
+    }
 
-        if (other == boid) return true;
 
-        Vector3 otherDirection = (other.position - boid.position);
 
-        Vector3 lookDirection = boidRigidbody.velocity;
+    private bool BoidVisible(Vector3 boidPosition, Vector3 boidForward, Vector3 checkPosition)
+    {
+        Vector3 otherDirection = (checkPosition - boidPosition);
 
-        float angle = Vector3.Angle(lookDirection, otherDirection);
+        float angle = Vector3.Angle(boidForward, otherDirection);
 
         return angle < 0.5f * boidFov;
     }
