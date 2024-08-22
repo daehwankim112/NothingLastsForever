a630d39ca866956c652baca289d9b68e44db48bd
Anthony Cieri <penguinmillion@gmail.com>
2024-08-21 18:22:10 -0700

    chore: refactor Boid manager compute

--- Assets/Scripts/Boids/BoidManagerCompute.cs
+++ Assets/Scripts/Boids/BoidManagerCompute.cs
@@ -1,331 +1,295 @@
+
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.InteropServices;
+
 using UnityEngine;
 using UnityEngine.UIElements;
-using static BoidManager;
+
+
 
 public class BoidManagerCompute : MonoBehaviour
 {
-    [SerializeField]
-    // Scale the force of the boid manager
-    public float BoidFactor;
-
-    [SerializeField]
-    // Scale the force of the boid manager
-    public float MaxSpeed;
-
-    [SerializeField]
-    // Scale the force of the boid manager
-    public float MinSpeed;
+    private struct Boid
+    {
+        protected Vector3 position;
+        protected Vector3 velocity;
+        protected Vector3 AvoidCollisionDirection;
+        protected int FramesSinceLastCollisionCheck;
+        protected Transform transform;
+    }
 
 
-    [SerializeField]
-    // How aggressivly the boids will avoid each other
-    public float SeparationFactor;
 
-    [SerializeField]
-    // The distance boids will stay away from each other
-    public float SeparationRadius;
+    private struct BufferBoid
+    {
+        protected Vector3 position;
+        protected Vector3 velocity;
 
-    [SerializeField]
-    // Will the boids avoid boids they can't see
-    public bool SeparationUsesFov;
+        protected Vector3 force;
+    }
 
 
-    [SerializeField]
-    // How aggressivly boids will match the velocity of boids around them
-    public float AlignmentFactor;
 
-    [SerializeField]
-    // The max distance for alignment
-    public float AlignmentRadius;
+    [System.Serializable]
+    public struct BoidSettings
+    {
+        public float BoidFactor;
+        public float MaxSpeed;
+        public float MinSpeed;
+        public float MaxTurning;
+        public float boidFov;
 
-    [SerializeField]
-    // Will the boids align with boids they can't see
-    public bool AlignmentUsesFov;
+        public float SeparationFactor;
+        public float SeparationRadius;
+        public bool SeparationUsesFov;
+
+        public float AlignmentFactor;
+        public float AlignmentRadius;
+        public bool AlignmentUsesFov;
+
+        public float CohesionFactor;
+        public float CohesionRadius;
+        public bool CohesionUsesFov;
+
+        public float TargetFactor;
+        public float TargetRadius;
+        public bool TargetUsesFov;
+
+        public float AvoidFactor;
+        public float AvoidRadius;
+        public bool AvoidUsesFov;
+    }
 
 
-    [SerializeField]
-    // How aggressivly boids will stick together
-    public float CohesionFactor;
-
-    [SerializeField]
-    // The max distance for sticking together
-    public float CohesionRadius;
-
-    [SerializeField]
-    // Will the boids go to boids they can't see
-    public bool CohesionUsesFov;
+    public float collisionAvoidanceFactor;
+    public float collisionAvoidanceRadius;
+    public int framesBetweenCollisionChecks;
+    private int collisionCheckStagger = 0;
 
 
-    [SerializeField]
-    // How aggressivly boids will move towards the target
-    public float TargetFactor;
 
-    [SerializeField]
-    // The max distance for targeting
-    public float TargetRadius;
-
-    [SerializeField]
-    // Will the boids go to targets they can't see
-    public bool TargetUsesFov;
-
-    [SerializeField]
-    // Boid target mode
-    public BoidRuleMode targetMode;
-
-    [SerializeField]
-    // The targets boids will move towards
+    public BoidSettings boidSettings;
     public List<Transform> targets;
-
-
-    [SerializeField]
-    // How aggressivly boids will avoid avoids
-    public float AvoidFactor;
-
-    [SerializeField]
-    // The max distance for avoiding
-    public float AvoidRadius;
-
-    [SerializeField]
-    // Will the boids avoid avoids they can't see
-    public bool AvoidUsesFov;
-
-    [SerializeField]
-    // Boid avoid mode
-    public BoidRuleMode avoidMode;
-
-    [SerializeField]
-    // The things boids will move away from
     public List<Transform> avoids;
 
+    private readonly List<Boid> boids = new();
 
-    [SerializeField]
-    // Limit to how far boids can go
-    public float boundingRadius;
-
-    [SerializeField]
-    // When the boids start turning back to not go too far
-    public float boundingEffectDistance;
-
-
-    [SerializeField]
-    // The maximum force the boid manager will apply
-    public float maxForce;
-
-    [SerializeField]
-    // The minimum force the boid manager will apply
-    public float minForce;
-
-
-    [SerializeField]
-    // The range of angles boids will see other boids
-    public float boidFov;
-
-
-    [SerializeField]
-    // The list of boids this manager manages
-    public readonly List<Transform> boids = new();
-
-    private readonly List<Rigidbody> boidRigidbodies = new();
-
-
-    private List<int> frameBoids;
-    private List<Vector3> frameBoidPositions;
-    private List<Vector3> frameBoidVelocities;
-    private List<Vector3> frameTargetPositions;
-    private List<Vector3> frameAvoidPositions;
-
-
-    [SerializeField]
     public ComputeShader BoidComputeShader;
+    private ComputeBuffer settingsBuffer;
+    private ComputeBuffer boidBuffer;
+    private ComputeBuffer targetPositionsBuffer, avoidPositionsBuffer;
 
-    ComputeBuffer boidPositionsBuffer, boidVelocitiesBuffer, targetPositionsBuffer, avoidPositionsBuffer, boidForcesBuffer;
-
-
-
-    //void Start()
-    //{
-    //    boids = new List<Transform>();
-    //    boidRigidbodies = new List<Rigidbody>();
-    //}
-
-
-
-    /// <summary>
-    /// Add a boid.
-    /// </summary>
-    /// <param name="boid">
-    /// The boid to add.
-    /// </param>
-    public void AddBoid(Transform boid)
-    {
-        if (boid == null) return;
-
-        if (!boid.TryGetComponent<Rigidbody>(out Rigidbody boidRigidbody)) return;
-
-        boids.Add(boid);
-        boidRigidbodies.Add(boidRigidbody);
-    }
-
-
-
-    /// <summary>
-    /// Remove a boid.
-    /// </summary>
-    /// <param name="boid">
-    /// The boid to remove.
-    /// </param>
-    /// <returns>
-    /// True if the boid was successfully removed.
-    /// False otherwise.
-    /// </returns>
-    public bool RemoveBoid(Transform boid)
-    {
-        Rigidbody boidRigidbody = boid.GetComponent<Rigidbody>();
-
-        return boids.Remove(boid)
-            && boidRigidbodies.Remove(boidRigidbody);
-    }
-
+    private BufferBoid[] bufferBoids;
 
 
 
     void FixedUpdate()
     {
+        if (boids.Count == 0) return;
+
+        FillBuffers();
+
+        ComputeBoidShader();
+
         UpdateBoids();
-    }
-
-
-
-    private void UpdateBoids()
-    {
-        // Fill frame lists
-        int numBoids = boids.Count;
-        int numTargets = targets.Count;
-        int numAvoids = avoids.Count;
-
-        if (numBoids == 0) return;
-
-        frameBoids = Enumerable.Range(0, numBoids).ToList();
-        frameBoidPositions = boids.Select(boid => boid.position).ToList();
-        frameBoidVelocities = boids.Select(boid => boid.GetComponent<Rigidbody>().velocity).ToList();
-        frameTargetPositions = targets.Select(target => target.position).ToList();
-        frameAvoidPositions = avoids.Select(avoid => avoid.position).ToList();
-
-
-        boidPositionsBuffer = new ComputeBuffer(numBoids, sizeof(float) * 3);
-        boidVelocitiesBuffer = new ComputeBuffer(numBoids, sizeof(float) * 3);
-        targetPositionsBuffer = new ComputeBuffer(numTargets, sizeof(float) * 3);
-        avoidPositionsBuffer = new ComputeBuffer(numAvoids, sizeof(float) * 3);
-
-        boidForcesBuffer = new ComputeBuffer(numBoids, sizeof(float) * 3);
-
-
-        boidPositionsBuffer.SetData(frameBoidPositions.ToArray());
-        boidVelocitiesBuffer.SetData(frameBoidVelocities.ToArray());
-        targetPositionsBuffer.SetData(frameTargetPositions.ToArray());
-        avoidPositionsBuffer.SetData(frameAvoidPositions.ToArray());
-
-
-        BoidComputeShader.SetFloat("separationRadius", SeparationRadius);
-        BoidComputeShader.SetFloat("alignmentRadius", AlignmentRadius);
-        BoidComputeShader.SetFloat("cohesionRadius", CohesionRadius);
-        BoidComputeShader.SetFloat("targetRadius", TargetRadius);
-        BoidComputeShader.SetFloat("avoidRadius", AvoidRadius);
-
-
-        BoidComputeShader.SetFloat("separationFactor", SeparationFactor);
-        BoidComputeShader.SetFloat("alignmentFactor", AlignmentFactor);
-        BoidComputeShader.SetFloat("cohesionFactor", CohesionFactor);
-        BoidComputeShader.SetFloat("targetFactor", TargetFactor);
-        BoidComputeShader.SetFloat("avoidFactor", AvoidFactor);
-
-
-        BoidComputeShader.SetBool("separationFov", SeparationUsesFov);
-        BoidComputeShader.SetBool("alignmentFov", AlignmentUsesFov);
-        BoidComputeShader.SetBool("cohesionFov", CohesionUsesFov);
-        BoidComputeShader.SetBool("targetFov", TargetUsesFov);
-        BoidComputeShader.SetBool("avoidFov", AvoidUsesFov);
-
-        BoidComputeShader.SetFloat("halfFovRad", 0.5f * boidFov * Mathf.Deg2Rad);
-
-
-        BoidComputeShader.SetBuffer(0, "boidPositions", boidPositionsBuffer);
-        BoidComputeShader.SetBuffer(0, "boidVelocities", boidVelocitiesBuffer);
-        BoidComputeShader.SetBuffer(0, "targetPositions", targetPositionsBuffer);
-        BoidComputeShader.SetBuffer(0, "avoidPositions", avoidPositionsBuffer);
-
-        BoidComputeShader.SetBuffer(0, "boidForces", boidForcesBuffer);
-
-
-        int threadGroups = Mathf.CeilToInt(numBoids / 256.0f);
-        BoidComputeShader.Dispatch(0, threadGroups, 1, 1);
-
-        Vector3[] forces = new Vector3[numBoids];
-        boidForcesBuffer.GetData(forces);
-
-
-        for (int boid = 0; boid < numBoids; boid++)
-        {
-            Vector3 boundingForce = maxForce * FindSafePath(frameBoidPositions[boid], frameBoidVelocities[boid]);
-            
-
-            Vector3 boidApplyForce = Vector3.ClampMagnitude(forces[boid] + boundingForce, maxForce);
-
-
-            if (boidApplyForce.sqrMagnitude < 0.0000001f)
-            {
-                boidApplyForce = minForce * frameBoidVelocities[boid].normalized;
-            }
-            else if (boidApplyForce.sqrMagnitude < minForce * minForce)
-            {
-                boidApplyForce = minForce * boidApplyForce.normalized;
-            }
-
-            //Debug.Log(forces[boid]);
-
-            boidRigidbodies[boid].AddForce(BoidFactor * boidApplyForce, ForceMode.Force);
-
-            Vector3 newVel = Vector3.ClampMagnitude(boidRigidbodies[boid].velocity, MaxSpeed);
-
-            if (newVel.sqrMagnitude < MinSpeed * MinSpeed)
-            {
-                newVel = MinSpeed * newVel.normalized;
-            }
-
-            boidRigidbodies[boid].velocity = newVel;
-        }
 
         ReleaseBuffers();
     }
 
 
-    private Vector3 FindSafePath(Vector3 position, Vector3 velocity)
+
+    private void OnDestroy()
+    {
+        ReleaseBuffers();
+    }
+
+
+
+    /// <summary>
+    /// Adds a new boid to the boid manager.
+    /// </summary>
+    /// <param name="boidTransform">The transform of the boid to add.</param>
+    /// <returns>True if the boid was successfully added, false otherwise.</returns>
+    public bool AddBoid(Transform boidTransform)
+    {
+        if (boidTransform == null) return false;
+
+        collisionCheckStagger++;
+
+        Boid newBoid = new()
+        {
+            position = boidTransform.position,
+            velocity = Vector3.zero,
+            AvoidCollisionDirection = Vector3.zero,
+            FramesSinceLastCollisionCheck = collisionCheckStagger % framesBetweenCollisionChecks,
+            transform = boidTransform
+        };
+        
+        boids.Add(newBoid);
+
+        return true;
+    }
+    
+
+
+    /// <summary>
+    /// Removes a boid from the boid manager.
+    /// </summary>
+    /// <param name="boid">The transform of the boid to be removed.</param>
+    /// <returns>True if the boid was successfully removed, false otherwise.</returns>
+    public bool RemoveBoid(Transform boid)
+    {
+        return boids.Remove(boid);
+    }
+
+
+
+    /// <summary>
+    /// Fills the buffers with boid settings, boid data, target positions, and avoid positions.
+    /// </summary>
+    private void FillBuffers()
+    {
+        int numBoids = boids.Count;
+        int numTargets = targets.Count;
+        int numAvoids = avoids.Count;
+
+
+        boidBuffer = new ComputeBuffer(numBoids, Marshal.SizeOf(typeof(BufferBoid)));
+        settingsBuffer = new ComputeBuffer(1, Marshal.SizeOf(typeof(BoidSettings)));
+        targetPositionsBuffer = new ComputeBuffer(numTargets, sizeof(float) * 3);
+        avoidPositionsBuffer = new ComputeBuffer(numAvoids, sizeof(float) * 3);
+
+
+        BufferBoid[] bufferBoids = new BufferBoid[boids.Count];
+        for (int i = 0; i < boids.Count; i++)
+        {
+            bufferBoids[i] = new BufferBoid
+            {
+                position = boids[i].transform.position,
+                velocity = boids[i].velocity,
+                force = Vector3.zero
+            };
+        }
+
+
+        boidBuffer.SetData(bufferBoids);
+        settingsBuffer.SetData(new BoidSettings[] { boidSettings });
+        targetPositionsBuffer.SetData(targets.Select(target => target.position).ToArray());
+        avoidPositionsBuffer.SetData(avoids.Select(avoid => avoid.position).ToArray());
+
+
+        BoidComputeShader.SetBuffer(0, "boids", boidBuffer);
+        BoidComputeShader.SetBuffer(0, "boidSettings", settingsBuffer);
+        BoidComputeShader.SetBuffer(0, "targetPositions", targetPositionsBuffer);
+        BoidComputeShader.SetBuffer(0, "avoidPositions", avoidPositionsBuffer);
+    }
+
+
+
+    private void ComputeBoidShader()
+    {
+        int numBoids = boids.Count;
+
+        // Calculate the number of thread groups needed based on the number of boids
+        int threadGroups = Mathf.CeilToInt((float) numBoids / 256.0f);
+        BoidComputeShader.Dispatch(0, threadGroups, 1, 1);
+
+        // Get the data from the boid buffer
+        bufferBoids = new BufferBoid[numBoids];
+        boidBuffer.GetData(bufferBoids);
+    }
+    
+
+    
+    /// <summary>
+    /// Updates the behavior of the boids.
+    /// </summary>
+    private void UpdateBoids()
+    {
+        int numBoids = boids.Count;
+
+        // Update each boid's position and velocity
+        for (int boidIndex = 0; boidIndex < numBoids; boidIndex++)
+        {
+            Boid boid = boids[boidIndex];
+
+            // Calculate the force acting on the boid
+            Vector3 force = bufferBoids[boidIndex].force;
+
+            boid.FramesSinceLastCollisionCheck++;
+            Vector3 avoidCollisionDirection = Vector3.zero;
+            if (boid.FramesSinceLastCollisionCheck >= framesBetweenCollisionChecks)
+            {
+                avoidCollisionDirection = FindSafePath(boid.position, boid.velocity);
+                boid.AvoidCollisionDirection = avoidCollisionDirection;
+                boid.FramesSinceLastCollisionCheck = 0;
+            }
+            
+            force += boidSettings.collisionAvoidanceFactor * boid.AvoidCollisionDirection;
+            force = Vector3.ClampMagnitude(force, boidSettings.MaxTurning);
+
+            // Update the velocity of the boid
+            Vector3 velocity = boid.velocity;
+            velocity += Time.FixedUpdate * force;
+
+            // Calculate the speed of the boid
+            float speed = velocity.Magnitude;
+
+            // Adjust the velocity based on the speed limits
+            if (speed <= 0.00001f)
+            {
+                velocity = force;
+            }
+            else if (speed < boidSettings.MinSpeed)
+            {
+                velocity = (velocity / speed) * boidSettings.MinSpeed;
+            }
+            else if (speed > boidSettings.MaxSpeed)
+            {
+                velocity = (velocity / speed) * boidSettings.MaxSpeed;
+            }
+
+            // Update the position of the boid
+            boid.position += Time.FixedUpdate * velocity;
+        }
+    }
+
+
+
+    /// <summary>
+    /// Finds a safe path for a boid to avoid collisions.
+    /// </summary>
+    /// <param name="position">The current position of the boid.</param>
+    /// <param name="velocity">The current velocity of the boid.</param>
+    /// <param name="numCircles">The number of circles to search for safe paths.</param>
+    /// <param name="searchesPerCircle">The number of searches to perform per circle.</param>
+    /// <returns>The safe path for the boid to follow.</returns>
+    private Vector3 FindSafePath(Vector3 position, Vector3 velocity, int numCircles = 4, int searchesPerCircle = 4)
     {
         Vector3 safePath = Vector3.zero;
 
-
-        if (!Physics.Raycast(position, velocity, out RaycastHit hit, boundingRadius)) return safePath;
+        // Check is forward path is safe
+        if (!Physics.Raycast(position, velocity, out RaycastHit hit, boidSettings.collisionAvoidanceRadius))
+        {
+            return safePath;
+        }
 
         float farthestDistance = hit.distance;
-
         Vector3 forwardDirection = velocity.normalized;
-
         Vector3 orthogonalDirection = Vector3.Cross(forwardDirection, hit.normal);
 
-
-
-        int numSearches = 6;
-        int numDirections = 6;
-        for (int i = numSearches; i > 1; i--)
+        // Iterate through search vectors
+        for (int i = numCircles; i > 1; i--)
         {
-            float searchAngle = (float) ((i + (numSearches / 2)) % numSearches) * 180.0f / (float) numSearches;
+            // Start at 90 degrees, search forward, then start backwards and search to 90 degrees
+            float searchAngle = (float) ((i + (numCircles / 2)) % numCircles) * 180.0f / (float) numCircles;
 
-            for (int j = 0; j < numDirections; j++)
+            for (int j = 0; j < searchesPerCircle; j++)
             {
-                float searchDirection = (float) j * 360.0f / (float) numDirections;
+                // Search around the full circle
+                float searchDirection = (float) j * 360.0f / (float) searchesPerCircle;
 
                 Vector3 testDirection = Quaternion.AngleAxis(searchDirection, forwardDirection)
                                       * (Quaternion.AngleAxis(searchAngle, orthogonalDirection)
@@ -341,8 +305,7 @@ public class BoidManagerCompute : MonoBehaviour
                 }
                 else
                 {
-                    safePath = testDirection;
-                    break;
+                    return testDirection;
                 }
             }
         }
@@ -352,27 +315,27 @@ public class BoidManagerCompute : MonoBehaviour
 
 
 
-    private void OnDestroy()
-    {
-        ReleaseBuffers();
-    }
-
+    /// <summary>
+    /// Releases the buffers used by the BoidManagerCompute.
+    /// </summary>
     private void ReleaseBuffers()
     {
-        if (boidPositionsBuffer != null)
+        if (boidBuffer != null)
         {
-            boidPositionsBuffer.Release();
-            boidPositionsBuffer = null;
+            boidBuffer.Release();
+            boidBuffer = null;
         }
-        if (boidVelocitiesBuffer != null)
+
+        if (targetPositionsBuffer != null)
         {
-            boidVelocitiesBuffer.Release();
-            boidVelocitiesBuffer = null;
+            targetPositionsBuffer.Release();
+            targetPositionsBuffer = null;
         }
-        if (boidForcesBuffer != null)
+
+        if (avoidPositionsBuffer != null)
         {
-            boidForcesBuffer.Release();
-            boidForcesBuffer = null;
+            avoidPositionsBuffer.Release();
+            avoidPositionsBuffer = null;
         }
     }
 }
