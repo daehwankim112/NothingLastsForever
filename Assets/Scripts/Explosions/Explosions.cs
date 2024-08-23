ea233bc642d02edd316fbe4970fd656a7f854c1b
Anthony Cieri <penguinmillion@gmail.com>
2024-08-23 15:11:29 -0700

    feat !: Removed Explosions.ExplosionAlliance move to GameManager.Alliance!!!

--- Assets/Scripts/Explosions/Explosions.cs
+++ Assets/Scripts/Explosions/Explosions.cs
@@ -1,16 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using static TorpedoManager;
 
 public static class Explosions
 {
-    public delegate void ExplosionEvent(Vector3 position, float power, ExplosionAlliance explosionAlliance);
-
-
-    public enum ExplosionAlliance
-    {
-        Player,
-        Enemy
-    }
+    public delegate void ExplosionEvent(Vector3 position, float power, GameManager.Alliance explosionAlliance);
 }
