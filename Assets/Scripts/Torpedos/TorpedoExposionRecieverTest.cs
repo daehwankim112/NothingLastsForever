9da6768439dc018beb5ee9c098acae383c3b54e7
Anthony Cieri <penguinmillion@gmail.com>
2024-08-22 21:06:47 -0700

    feat: Separated the explosion event from the torpedo class

--- Assets/Scripts/Torpedos/TorpedoExposionRecieverTest.cs
+++ Assets/Scripts/Torpedos/TorpedoExposionRecieverTest.cs
@@ -5,19 +5,20 @@ public class TorpedoExposionRecieverTest : MonoBehaviour
 {
     private void Start()
     {
-        var torpedoManager = FindObjectOfType<TorpedoManager>();
+        TorpedoManager torpedoManager = FindObjectOfType<TorpedoManager>();
+
         if (torpedoManager == null) return;
 
         torpedoManager.OnTorpedoExploded += OnTorpedoExploded;
     }
 
-    private void OnTorpedoExploded(Vector3 position, float power, TorpedoManager.TorpedoAlliance alliance)
+    private void OnTorpedoExploded(Vector3 position, float power, Explosions.ExplosionAlliance alliance)
     {
         float distance = Vector3.Distance(position, transform.position);
 
         float explosionPower = power / (distance * distance);
 
-        Debug.Log($"Torpedo of type {(alliance == TorpedoManager.TorpedoAlliance.Player ? "Player" : "Enemy")} exploded at {position} with power {power}, felt as {explosionPower:F2} {distance:F2} meters away");
+        Debug.Log($"Torpedo of type {(alliance == Explosions.ExplosionAlliance.Player ? "Player" : "Enemy")} exploded at {position} with power {power}, felt as {explosionPower:F2} {distance:F2} meters away");
     }
 
 
