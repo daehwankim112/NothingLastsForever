66ab5f92d7b11f9edcf24369cdb01ff990e6e9cd
Anthony Cieri <penguinmillion@gmail.com>
2024-08-23 14:44:07 -0700

    feat: Inventory and collectables skeleton scripts

---
+++ Assets/Scripts/InventorySystem/Inventory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int NumTorpedos = 0;
+
+    public int Health = 100;
+}
