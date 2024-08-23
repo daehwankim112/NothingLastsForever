a9746cbb2defe70ea38a10afc3a0ec6d861300a6
Anthony Cieri <penguinmillion@gmail.com>
2024-08-23 16:59:01 -0700

    feat: Add events to game manager

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,10 +1,46 @@
 
+using System;
+
 using UnityEngine;
-using static Explosions;
 
 public class GameManager : Singleton<GameManager>
 {
-    public delegate void DeathEvent(GameObject deadThing, Alliance alliance);
+    /// <summary>
+    /// Death Event
+    /// </summary>
+    #region Death Event
+    public class OnDeathArgs : EventArgs
+    {
+        public GameObject DeadThing;
+        public Alliance DeadThingAlliance;
+    }
+    public static event EventHandler<OnDeathArgs> OnDeath;
+
+    public static void Death(GameObject deadThing, Alliance alliance)
+    {
+        OnDeath?.Invoke(null, new OnDeathArgs { DeadThing = deadThing, DeadThingAlliance = alliance });
+    }
+    #endregion
+
+
+    /// <summary>
+    /// Explosion Event
+    /// </summary>
+    #region Explosion Event
+    public class OnExplosionArgs : EventArgs
+    {
+        public Vector3 Position;
+        public float Power;
+        public Alliance ExplosionAlliance;
+    }
+    public static event EventHandler<OnExplosionArgs> OnExplosion;
+
+    public static void Explosion(Vector3 position, float power, Alliance alliance)
+    {
+        OnExplosion?.Invoke(null, new OnExplosionArgs { Position = position, Power = power, ExplosionAlliance = alliance });
+    }
+    #endregion
+
 
 
     public enum Alliance
