a09db24549d633eaf3bcfe5691deaca4933d5918
Anthony Cieri <penguinmillion@gmail.com>
2024-08-23 18:15:35 -0700

    feat: Collection manager death detection

--- Assets/Scripts/InventorySystem/CollectablesManager.cs
+++ Assets/Scripts/InventorySystem/CollectablesManager.cs
@@ -1,8 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class CollectablesManager : MonoBehaviour
-{
 
+
+public class CollectablesManager : Singleton<CollectablesManager>
+{
+    private GameManager gameManager => GameManager.Instance;
+
+
+
+    void Start()
+    {
+        gameManager.OnDeath += OnDeath;
+    }
+
+
+
+    private void OnDestroy()
+    {
+        gameManager.OnDeath -= OnDeath;
+    }
+
+
+
+    private void OnDeath(object sender, GameManager.OnDeathArgs deathArgs)
+    {
+        if (!deathArgs.DeadThing.TryGetComponent<Inventory>(out Inventory inventory))
+        {
+            Debug.Log($"CollectablesManager: {deathArgs.DeadThing.name} died, dropping {inventory.NumTorpedos} torpedos");
+        }
+    }
 }
